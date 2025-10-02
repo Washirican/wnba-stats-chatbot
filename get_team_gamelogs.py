88464a9e767107b14b84064aad6aedb8dfa4b343
Daniel A. Rodriguez-Delgado <daniel.a.rodriguez-delgado@boeing.com>
2025-10-02 15:13:31 -0700

    Revise request headers.

--- get_team_gamelogs.py
+++ get_team_gamelogs.py
@@ -8,26 +8,12 @@ import requests
 import json
 import csv
 
-# HEADERS = {
-#     'Host': 'stats.wnba.com',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0',
-#     'Accept': 'application/json, text/plain, */*',
-#     'Accept-Language': 'en-US,en;q=0.9',
-#     'Accept-Encoding': 'gzip, deflate, br, zstd',
-#     'x-nba-stats-origin': 'stats',
-#     'x-nba-stats-token': 'true',
-#     'Connection': 'keep-alive',
-#     'Referer': 'https://stats.wnba.com/teams/',
-#     'Pragma': 'no-cache',
-#     'Cache-Control': 'no-cache',
-# }
-
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)...',
     'Accept': 'application/json, text/plain, */*',
     'x-nba-stats-origin': 'stats',
     'x-nba-stats-token': 'true',
-    'Referer': 'https://stats.wnba.com/teams/',
+    'Referer': 'https://stats.wnba.com/',
 }
 
 # request_url = 'https://stats.wnba.com/stats/teamgamelogs?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=10&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=Totals&Period=0&PlusMinus=N&Rank=N&Season=2025&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&VsConference=&VsDivision='
@@ -39,6 +25,8 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%d-%b-%y %H:%M:%S')
 
 logging.disable(logging.CRITICAL)
+
+SEASON = '1997'
 
 """Get player season gamelog."""
 parameters = {
@@ -53,7 +41,7 @@ parameters = {
     'Period': '0',
     'PlusMinus': 'N',
     'Rank': 'N',
-    'Season': '2025',
+    'Season': SEASON,
     'SeasonType': 'Regular Season',
     'TeamID': '0',
     'PlayerID': '0',
@@ -71,7 +59,7 @@ headers = json.loads(r.content.decode())['resultSets'][0]['headers']
 data = json.loads(r.content.decode())['resultSets'][0]['rowSet']
 
 # Open a file to write that heat
-with open('data/team_gamelogs.csv', 'w', newline='', encoding='utf-8') as file:
+with open(f'data/team_gamelogs_{SEASON}.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(headers) 
     writer.writerows(data)
